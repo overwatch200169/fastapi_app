8a4741ff122a8d57c2f00052ee16c68fc69a284a
overwatch20019 <1091587398@qq.com>
2026-04-21 02:20:31 +0800

    增加列出所有文章的区分用户等级的API，增加恢复文章API，为删除文章API做用户等级划分

--- app/api/v1/article.py
+++ app/api/v1/article.py
@@ -6,9 +6,13 @@ from app.schemas.articles import ArticlePublic, ArticleList, ArticleCreate, Arti
 from app.schemas.users import UserPublic
 # from app.utils.deps import ArticleServiceDep, get_current_active_user
 from app.dependencies.article import ArticleServiceDep
-from app.dependencies.auth import  get_current_active_user_dep
+from app.dependencies.auth import get_current_active_user_dep, need_admin_dep
 
 router=APIRouter(tags=['Article'])
+
+@router.get('/all',response_model=list[ArticleList])
+async def list_article_admin(service:ArticleServiceDep,user:need_admin_dep):
+    return service.list_articles(user_level=user.level)
 
 @router.get('/{article_id}',response_model=ArticlePublic)
 async def read_article(article_id:int,service:ArticleServiceDep):
@@ -20,6 +24,9 @@ async def read_article(article_id:int,service:ArticleServiceDep):
 @router.get('/',response_model=list[ArticleList])
 async def list_article(service:ArticleServiceDep):
     return service.list_articles()
+
+
+
 @router.post('/',response_model=ArticlePublic)
 async def create_article(article:ArticleCreate,service:ArticleServiceDep,user:get_current_active_user_dep):
     article_data=service.create_article(article, user)
@@ -36,16 +43,25 @@ async def article_update(article_id, service:ArticleServiceDep, user:get_current
         raise HTTPException (status_code=403,detail='update fail')
     return update
 
+@router.patch('/recovery/{article_id}')
+async def article_recovery(article_id, service:ArticleServiceDep, user:need_admin_dep):
+    recover=service.recover_article(user_level=user.level, article_id=article_id)
+    if not recover:
+        raise HTTPException (status_code=403,detail='update fail')
+    return recover
+
 
 
 
 
 @router.delete('/{article_id}')
 async def delete_article(article_id,service:ArticleServiceDep,user:get_current_active_user_dep):
-    is_removed=service.remove_article(article_id,user.user_id)
-    await service.delete_article_search_by_id(article_id)
+    is_removed=service.remove_article(article_id,user.user_id,user.level)
+
     if not is_removed:
         raise HTTPException(status_code=403,detail='remove fail')
+    else:
+        await service.delete_article_search_by_id(article_id)
     return is_removed
     # pass
 
