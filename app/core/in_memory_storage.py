e0a677d6b29a257d35ef9edb89e20d8f40a51a76
overwatch20019 <1091587398@qq.com>
2026-04-24 02:06:13 +0800

    内存存储实现

---
+++ app/core/in_memory_storage.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+
+from app.core.storage import CaptchaStorage
+class MemoryStorage(CaptchaStorage):
+    def __init__(self):
+        self._storage = {}
+    async def set(self,key,code,ttl):
+        expire_at=datetime.now()+timedelta(seconds=ttl)
+        self._storage[key]={'captcha_code':code,
+            'expires_at':expire_at}
+        print(self._storage)
+
+    async def get(self,key):
+        if key not in self._storage:
+            return None
+        data=self._storage[key]
+        print('code,expire at',data)
+        if datetime.now()>data['expires_at']:
+            self._storage.pop(key,None)
+            return None
+        return data['captcha_code']
+    async def delete(self,key):
+        self._storage.pop(key,None)
+
